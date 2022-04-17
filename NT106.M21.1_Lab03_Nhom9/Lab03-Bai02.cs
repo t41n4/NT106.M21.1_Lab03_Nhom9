ba82712b609261591cd68b06bff8688bd7ed0114
Rabbithole07 <102034724+tranhunghoan@users.noreply.github.com>
2022-04-18 01:48:39 +0700

    Bai2 update

--- NT106.M21.1_Lab03_Nhom9/Lab03-Bai02.cs
+++ NT106.M21.1_Lab03_Nhom9/Lab03-Bai02.cs
@@ -1,4 +1,9 @@
 ﻿using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
 
 namespace NT106.M21._1_Lab03_Nhom9
 {
@@ -9,14 +14,73 @@ namespace NT106.M21._1_Lab03_Nhom9
             InitializeComponent();
         }
 
-        private void lv__SelectedIndexChanged(object sender, System.EventArgs e)
-        {
 
-        }
 
         private void btn_Listen_Click(object sender, System.EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
+            TcpListener server = null;
+            try
+            {
+                int port = 9090;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop. 
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests. 
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Hello!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client. 
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+            
+            }
         }
     }
 }
